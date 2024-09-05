66a96f2a09d08817dda55b3027581a35e2d01847
Pascual Camacho <pascualc02@gmail.com>
2024-09-04 20:21:49 -0700

    update links

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,44 +1,43 @@
 type Social = {
-  label: string;
-  link: string;
+    label: string;
+    link: string;
 };
 
 type Presentation = {
-  mail: string;
-  title: string;
-  description: string;
-  socials: Social[];
-  profile?: string;
+    mail: string;
+    title: string;
+    description: string;
+    socials: Social[];
+    profile?: string;
 };
 
 const presentation: Presentation = {
-  mail: "me@pascualcam.com",
-  title: "Hi, I’m Pascual 😁",
-  // profile: "/profile.webp",
-  description:
-    "*Software Engineer* and tech enthusiast.",
-  socials: [
-    {
-      label: "X",
-      link: "https://twitter.com/pascualcmch",
-    },
-    {
-      label: "Linkedin",
-      link: "https://linkedin.com/in/pascualcam",
-    },
-    {
-      label: "Github",
-      link: "https://github.com/pascualcam",
-    },
-    {
-      label: "Literal",
-      link: "https://literal.club/ecam",
-    },
-    {
-      label: "Strava",
-      link: "https://www.strava.com/athletes/18664359",
-    },
-  ],
+    mail: "me@pascualcam.com",
+    title: "Hi, I’m Pascual 😁",
+    // profile: "/profile.webp",
+    description: "*Software Engineer* and tech enthusiast.",
+    socials: [
+        {
+            label: "X",
+            link: "https://twitter.com/pascualcmch",
+        },
+        {
+            label: "Linkedin",
+            link: "https://linkedin.com/in/pascualcam",
+        },
+        {
+            label: "Github",
+            link: "https://github.com/pascualcam",
+        },
+        {
+            label: "Literal",
+            link: "https://literal.club/ecam",
+        },
+        {
+            label: "Strava",
+            link: "https://www.strava.com/athletes/18664359",
+        },
+    ],
 };
 
 export default presentation;
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,43 +1,43 @@
 export type Project = {
-  title: string;
-  techs: string[];
-  link: string;
-  isComingSoon?: boolean;
+    title: string;
+    techs: string[];
+    link: string;
+    isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
-  {
-    title: "Kuwi - a family tree generator",
-    techs: ["Python", "SQLAlchemy", "PostgreSQL"],
-    link: "https://kuwiapp.com",
-  },
-  {
-    title: "ML at Stanford",
-    techs: ["Pandas", "Jupyter"],
-    link: "https://github.com/pascualcam",
-  },
-  {
-    title: "World Cup historical data",
-    techs: ["Python"],
-    link: "https://github.com/pascualcam",
-    isComingSoon: true,
-  },
-  {
-    title: "Tour de France",
-    techs: ["Python"],
-    link: "https://github.com/pascualcam/tour-de-tour",
-    isComingSoon: true,
-  },
-  {
-    title: "Algorithms",
-    techs: ["Python", "Java", "C", "MATLAB"],
-    link: "https://github.com/pascualcam/algorithms",
-  },
-  {
-    title: "This website",
-    techs: ["TailwindCSS", "TypeScript"],
-    link: "https://pascualcam.com",
-  },
+    {
+        title: "Kuwi - a family tree generator",
+        techs: ["Python", "SQLAlchemy", "PostgreSQL"],
+        link: "https://kuwiapp.com",
+    },
+    {
+        title: "ML at Stanford",
+        techs: ["Pandas", "Jupyter"],
+        link: "https://github.com/pascualcam",
+    },
+    {
+        title: "World Cup historical data",
+        techs: ["Python"],
+        link: "https://github.com/pascualcam",
+        isComingSoon: true,
+    },
+    {
+        title: "Tour de France",
+        techs: ["Python"],
+        link: "https://github.com/pascualcam/tour-de-tour",
+        isComingSoon: true,
+    },
+    {
+        title: "Algorithms",
+        techs: ["Python", "Java", "C", "MATLAB"],
+        link: "https://github.com/pascualcam/algorithms",
+    },
+    {
+        title: "This website",
+        techs: ["TailwindCSS", "TypeScript"],
+        link: "https://pascualcam.com",
+    },
 ];
 
 export default projects;
